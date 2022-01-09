5753fe325a9001fd15ebcf55e63b752617dcafbf
Macktireh <abdimack97@gmail.com>
2022-01-09 21:52:51 +0100

    Mise en place de l'application AppPortfolio, ajout des fichiers templates(html django) et static(image, css avec le processeur sass et javascript. Création de la page d'acceuil. Insha'Allah à la prochaine. Merci

---
+++ src/AppPortfolio/static/AppPortfolio/js/home.js
@@ -0,0 +1,17 @@
+/* =================== Button pour revenir en haut =================== */
+
+// const BtnTop = document.querySelector(".btn-top");
+
+// window.addEventListener("scroll", () => {
+//   if (window.scrollY > 650) {
+//     BtnTop.classList.add("btn-top-show");
+//   } else BtnTop.classList.remove("btn-top-show");
+// });
+
+// BtnTop.addEventListener("click", () => {
+//   window.scrollTo({
+//     top: 0,
+//     left: 0,
+//     behavior: "smooth",
+//   });
+// });
--- src/MySitePortfolio/settings.py
+++ src/MySitePortfolio/settings.py
@@ -11,9 +11,11 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
 
 # Quick-start development settings - unsuitable for production
@@ -37,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'AppPortfolio',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +57,7 @@ ROOT_URLCONF = 'MySitePortfolio.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -103,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'fr-fr'
 
 TIME_ZONE = 'UTC'
 
@@ -118,6 +121,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES = [
+    os.path.join(BASE_DIR, 'AppPortfolio', 'static')
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
