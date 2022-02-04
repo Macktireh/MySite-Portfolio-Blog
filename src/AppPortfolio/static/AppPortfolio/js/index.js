19a9c41920b714c13af72a176d7366bb13d2c6ef
Macktireh <abdimack97@gmail.com>
2022-02-04 22:01:13 +0100

    ajout de responsive navbar

--- src/AppPortfolio/static/AppPortfolio/js/index.js
+++ src/AppPortfolio/static/AppPortfolio/js/index.js
@@ -1,3 +1,15 @@
+/* =================== Responsive navbar =================== */
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+const navLink = document.querySelector(".nav-link");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+  navLink.classList.toggle("active");
+});
+
 /* =================== Button pour remonter en haut de la page =================== */
 
 const BtnTop = document.querySelector(".btn-top");
