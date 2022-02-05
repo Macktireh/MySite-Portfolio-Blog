28b369016969b3a288fcc6124f269b885bab3975
Macktireh <abdimack97@gmail.com>
2022-02-05 21:24:07 +0100

    Organisation et structure du projet

---
+++ src/apps/AppPortfolio/views.py
@@ -0,0 +1,52 @@
+from django.shortcuts import render
+from .models import Project, Competence
+
+
+def home(request):
+    
+    # récuperer toutes les données depuis la base de données
+    Projects = Project.objects.filter(favorites=True)
+    Competences = Competence.objects.all()
+    
+    # contexte : passer les données au gabarite de django
+    contexte = {
+        'Projects': Projects,
+        'Competences': Competences
+    }
+    
+    # le schema du document à rendre ou afficher 
+    template = 'portfolio/pages/home.html'
+    
+    return render(request, template, contexte)
+
+
+def project(request):
+    
+    # récuperer toutes les données depuis la base de données
+    Projects = Project.objects.all()
+    
+    # contexte : passer les données au gabarite de django
+    contexte = {
+        'Projects': Projects,
+    }
+    
+    # le schema du document à rendre ou afficher 
+    template = 'portfolio/pages/project.html'
+    
+    return render(request, template, contexte)
+
+
+def detail_project(request, slug):
+    
+    # récuperer toutes les données depuis la base de données
+    project = Project.objects.get(slug=slug)
+    
+    # contexte : passer les données au gabarite de django
+    contexte = {
+        'Project': project,
+    }
+    
+    # le schema du document à rendre ou afficher 
+    template = 'portfolio/pages/detail_project.html'
+    
+    return render(request, template, contexte)
