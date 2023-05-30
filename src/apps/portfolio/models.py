888450797b215f8cb4dde188e2422fd57890bf75
Macktireh ABDI SOUBANEH <macktireh.abdi-soubaneh@external.totalenergies.com>
2023-05-30 17:34:37 +0200

    upgrade project

--- src/apps/portfolio/models.py
+++ src/apps/portfolio/models.py
@@ -3,7 +3,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.template.defaultfilters import slugify
 from django.utils import timezone
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 from ckeditor.fields import RichTextField
 
 
